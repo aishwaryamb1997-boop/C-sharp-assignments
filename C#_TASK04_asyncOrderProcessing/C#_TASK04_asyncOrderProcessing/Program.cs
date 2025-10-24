dfbd2618c85279a8b3c97bd0a1693d645f0797e8
aishwaryamb1997-boop <aishwaryamb1997@gmail.com>
2025-10-24 17:39:57 +0530

    Add files via upload

---
+++ C#_TASK04_asyncOrderProcessing/C#_TASK04_asyncOrderProcessing/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading.Tasks; 
+
+namespace AsyncOrderProcessing
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("===== Online Order Processing =====");
+
+            
+            Console.Write("Enter the name of your order: ");
+            string orderName = Console.ReadLine();
+
+            
+            Console.WriteLine("\nProcessing order...");
+
+            
+            await ProcessOrderAsync(orderName);
+
+            Console.WriteLine($"Order for {orderName} is ready!");
+        }
+
+       
+        static async Task ProcessOrderAsync(string order)
+        {
+            
+            await Task.Delay(3000); 
+        }
+    }
+}
