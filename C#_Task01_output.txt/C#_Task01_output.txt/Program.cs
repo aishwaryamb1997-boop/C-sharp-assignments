b57daec9d323025f33b2ecc5441236a6519fe15e
aishwaryamb1997-boop <aishwaryamb1997@gmail.com>
2025-10-24 16:53:21 +0530

    Add files via upload

---
+++ C#_Task01_output.txt/C#_Task01_output.txt/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C__Task01_output.txt
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string folderPath = Path.Combine(
+                    Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "MyFiles");
+
+                string filePath = Path.Combine(folderPath, "output.txt");
+
+                if(!Directory.Exists(folderPath))
+                {
+                    Directory.CreateDirectory(folderPath);
+                    Console.WriteLine($"created new folder @ {folderPath}");
+                }
+                Console.WriteLine("Enter multiple lines of text (type 'STOP' to finish):");
+
+                using (StreamWriter writer = new StreamWriter(filePath))
+                {
+                    while(true)
+                    {
+                        string inputLine = Console.ReadLine();
+                        if (inputLine.ToUpper() == "STOP")
+                            break;
+
+                        writer.WriteLine(inputLine);
+                    }
+                }
+                Console.WriteLine($"\n Text has been saved @ {filePath}");
+
+                Console.WriteLine("Contents of the file:");
+                using (StreamReader reader = new StreamReader(filePath))
+                {
+                    string fileContent = reader.ReadToEnd();
+                    Console.WriteLine("------------------------------");
+                    Console.WriteLine(fileContent);
+                    Console.WriteLine("------------------------------");
+                }
+             
+            }
+            catch (IOException ioEx)
+            {
+                Console.WriteLine(ioEx.Message);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.ReadKey();
+        }
+    }
+}
